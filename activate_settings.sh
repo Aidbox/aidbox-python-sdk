f6b3965f89d4986b1f9a1abe26d227ec4b63ab09
mkizesov <miron-k@ya.ru>
2019-02-15 12:42:55 +0300

    Update activate_settings.sh

--- activate_settings.sh
+++ activate_settings.sh
@@ -3,7 +3,7 @@
 
 # the AIO_ env variables are used by `adev runserver` when serving your app for development
 export AIO_APP_PATH="aidbox_python_sdk/"
-
+export $(grep -v '^#' .env | xargs)
 
 # also activate the python virtualenv for convenience, you can remove this if you're python another way
 . env/bin/activate
