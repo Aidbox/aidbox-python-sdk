e8fa62a9e18c1e5079d209ffa2069d6ee771e37f
Dmitry Shutov <dmitryashutov@gmail.com>
2022-12-14 13:47:56 +0600

    Remove init_http_client utilization

--- aidbox_python_sdk/main.py
+++ aidbox_python_sdk/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import errno
+import json
 import logging
 import os
 import sys
@@ -11,6 +12,7 @@ from .aidboxpy import AsyncAidboxClient
 from .db import DBProxy
 from .handlers import routes
 from .sdk import SDK
+from fhirpy.base.exceptions import OperationOutcome
 
 logger = logging.getLogger("aidbox_sdk")
 THIS_DIR = Path(__file__).parent
@@ -21,36 +23,33 @@ def setup_routes(app):
     app.add_routes(routes)
 
 
-async def register_app(
-    sdk: SDK, *, http_client: ClientSession, aidbox_client: AsyncAidboxClient
-):
-    try:
-        async with http_client.put(
-            "{}/App".format(sdk.settings.APP_INIT_URL),
-            json={
-                "resourceType": "App",
-                "apiVersion": 1,
-                "type": "app",
-                "id": sdk.settings.APP_ID,
-                "endpoint": {
-                    "url": sdk.settings.APP_URL,
-                    "type": "http-rpc",
-                    "secret": sdk.settings.APP_SECRET,
-                },
-                **sdk.build_manifest(),
+async def register_app(sdk: SDK, client: AsyncAidboxClient):
+    app_resource = client.resource(
+        "App",
+        **{
+            "apiVersion": 1,
+            "type": "app",
+            "id": sdk.settings.APP_ID,
+            "endpoint": {
+                "url": sdk.settings.APP_URL,
+                "type": "http-rpc",
+                "secret": sdk.settings.APP_SECRET,
             },
-        ) as resp:
-            if 200 <= resp.status < 300:
-                logger.info("Initializing Aidbox app...")
-                await sdk.initialize(aidbox_client)
-            else:
-                logger.error(
-                    "Aidbox app initialized failed. "
-                    "Response from Aidbox: {0} {1}".format(
-                        resp.status, await resp.text()
-                    )
+            **sdk.build_manifest(),
+        }
+    )
+    try:
+        try:
+            await app_resource.save()
+            logger.info("Creating seeds and applying migrations")
+            await sdk.initialize(client)
+        except OperationOutcome as error:
+            logger.error(
+                "Error during the App registration: {}".format(
+                    json.dumps(error, indent=2)
                 )
-                sys.exit(errno.EINTR)
+            )
+            sys.exit(errno.EINTR)
     except (
         client_exceptions.ServerDisconnectedError,
         client_exceptions.ClientConnectionError,
@@ -61,15 +60,7 @@ async def register_app(
         sys.exit(errno.EINTR)
 
 
-async def init_http_client(app):
-    basic_auth = BasicAuth(
-        login=app["settings"].APP_INIT_CLIENT_ID,
-        password=app["settings"].APP_INIT_CLIENT_SECRET,
-    )
-    app["init_http_client"] = ClientSession(auth=basic_auth)
-
-
-async def init_aidbox_client(app):
+async def init_client(app):
     basic_auth = BasicAuth(
         login=app["settings"].APP_INIT_CLIENT_ID,
         password=app["settings"].APP_INIT_CLIENT_SECRET,
@@ -87,24 +78,15 @@ async def init_db(app):
 
 
 async def on_startup(app):
-    await init_http_client(app)
-    await init_aidbox_client(app)
+    await init_client(app)
     await init_db(app)
 
-    await register_app(
-        app["sdk"], http_client=app["init_http_client"], aidbox_client=app["client"]
-    )
+    await register_app(app["sdk"], app["client"])
 
 
 async def on_cleanup(app):
-    await app["init_http_client"].close()
     await app["db"].deinitialize()
     await app["sdk"].deinitialize()
-
-
-async def on_shutdown(app):
-    if not app["init_http_client"].closed:
-        await app["init_http_client"].close()
 
 
 def create_app(sdk: SDK):
@@ -112,7 +94,6 @@ def create_app(sdk: SDK):
     app = web.Application()
     app.on_startup.append(on_startup)
     app.on_cleanup.append(on_cleanup)
-    app.on_shutdown.append(on_shutdown)
     app.update(
         name="aidbox-python-sdk",
         settings=sdk.settings,
