ee684133c6add5ee905f3f074057d149ac848e60
Dmitry Shutov <dmitryashutov@gmail.com>
2022-12-19 14:52:28 +0600

    Update meta info in the package __ini__

--- aidbox_python_sdk/__init__.py
+++ aidbox_python_sdk/__init__.py
@@ -1,8 +1,8 @@
-__title__ = "aidbox_python_sdk"
-__version__ = "0.0.1-alpha"
+__title__ = "aidbox-python-sdk"
+__version__ = "0.1.0"
 __author__ = "beda.software"
 __license__ = "None"
-__copyright__ = "Copyright 2020 beda.software"
+__copyright__ = "Copyright 2022 beda.software"
 
 # Version synonym
 VERSION = __version__
