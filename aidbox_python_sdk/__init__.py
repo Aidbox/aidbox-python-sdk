d2c594886573364e8697fb8c4692434a08537fc6
Dmitry Shutov <dmitryashutov@gmail.com>
2023-01-03 18:03:40 +0600

    Bump version to 0.1.1

--- aidbox_python_sdk/__init__.py
+++ aidbox_python_sdk/__init__.py
@@ -1,8 +1,8 @@
 __title__ = "aidbox-python-sdk"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "beda.software"
 __license__ = "None"
-__copyright__ = "Copyright 2022 beda.software"
+__copyright__ = "Copyright 2023 beda.software"
 
 # Version synonym
 VERSION = __version__
