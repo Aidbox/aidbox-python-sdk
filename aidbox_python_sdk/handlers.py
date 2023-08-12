00a2644d92cd41961e925660b2c50f91bae24fdd
Vadim Laletin <ruscoder@users.noreply.github.com>
2023-08-12 09:11:28 +0200

    Add app to request dict for subscriptions

--- aidbox_python_sdk/handlers.py
+++ aidbox_python_sdk/handlers.py
@@ -17,7 +17,8 @@ async def subscription(request, data):
     if not handler:
         logger.error("Subscription handler `{}` was not found".format(data["handler"]))
         raise web.HTTPNotFound()
-    result = handler(data["event"], request)
+    data["request"]["app"] = request.app
+    result = handler(data["event"], data["request"])
     if asyncio.iscoroutine(result):
         asyncio.get_event_loop().create_task(result)
     return web.json_response({})
