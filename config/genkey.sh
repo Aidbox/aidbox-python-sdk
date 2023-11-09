248015e8359595564b0b24b7489d7dfd8464e535
Ilya Beda <ir4y.ix@gmail.com>
2023-11-09 15:48:53 +1100

    Update aidbox
    Use zenproject for configuration

--- config/genkey.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -t rsa -b 4096 -f jwtRS256.key
-openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
-
--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yaml up --exit-code-from app app
+docker compose up --exit-code-from app app
