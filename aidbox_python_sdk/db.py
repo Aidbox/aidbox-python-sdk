a28dc470cc24e79c5c1c984c33c798f7c994c333
Vadim Laletin <ruscoder@users.noreply.github.com>
2025-09-30 18:33:57 +0200

    Use $resource-types over Entity for db

--- aidbox_python_sdk/db.py
+++ aidbox_python_sdk/db.py
@@ -138,18 +138,19 @@ class DBProxy:
     async def _get_all_entities_name(self):
         result = None
 
-        #Support legacy instalations with entity attribute data structure
-        query_url = f"{self._settings.APP_INIT_URL}/Entity?type=resource&_elements=id&_count=999"
+        query_url = f"{self._settings.APP_INIT_URL}/$resource-types"
         async with self._client.get(query_url) as resp:
-            json_resp = await resp.json()
-            result = [entry["resource"]["id"] for entry in json_resp.get("entry", [])]
-
-        if result is None or len(result) == 0:
-            # Load resource type from fhir schema
-            query_url = f"{self._settings.APP_INIT_URL}/$resource-types"
-            async with self._client.get(query_url) as resp:
+            if resp.status == 200:
                 json_resp = await resp.json()
                 result = list(json_resp.keys())
+
+        if not result:
+            # Support legacy instalations with entity attribute data structure
+            query_url = f"{self._settings.APP_INIT_URL}/Entity?type=resource&_elements=id&_count=999"
+            async with self._client.get(query_url) as resp:
+                json_resp = await resp.json()
+                result = [entry["resource"]["id"] for entry in json_resp.get("entry", [])]
+
         return result or []
 
     async def _init_table_cache(self):
