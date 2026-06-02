78ff49c5a4af2fc1bd18b5890446a9d6e7c1dfd8
Vadim Laletin <rusmiligamer@gmail.com>
2026-06-02 23:10:54 +0200

    Bump to 0.2.3

--- aidbox_python_sdk/__init__.py
+++ aidbox_python_sdk/__init__.py
@@ -1,8 +1,8 @@
 __title__ = "aidbox-python-sdk"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "beda.software"
 __license__ = "None"
-__copyright__ = "Copyright 2024 beda.software"
+__copyright__ = "Copyright 2026 beda.software"
 
 # Version synonym
 VERSION = __version__
